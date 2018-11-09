b0672d1956f23bea1e5a8a1f5805572e2c87da9c
Gianluca Gross <ggross@seas.upenn.edu>
2018-11-09 14:19:44 -0500

    Added another null dereference bug

--- test_clang/test.c
+++ test_clang/test.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "helper.h"
 
 int helper2(int* p) {
@@ -6,7 +7,15 @@ int helper2(int* p) {
 }
 
 int main(int argc, char* argv[]) {
-    int* p = NULL;
+    int x = 23;
+    int* p = &x;
+    if (argc >= 2 && strcmp(argv[1], "test")) {
+        p = NULL;
+    }
     printf("The number is %d\n", helper2(p));
+
+    int* z = NULL;
+    int y = *z;
+
     return 0;
 }

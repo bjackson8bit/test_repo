f6b795f0ee397f598b3d0c2a54a30236852c7016
Gianluca Gross <ggross@seas.upenn.edu>
2018-11-28 23:42:30 -0500

    New bugs

--- test.c
+++ test.c
@@ -2,8 +2,6 @@
 #include <string.h>
 #include "helper.h"
 
-// Comment for diff
-
 int otherHelper(int* p) {
     return *p - 5;
 }
@@ -32,11 +30,9 @@ int main(int argc, char* argv[]) {
     printf("amazing\n");
 
     printf("num1 + 4 is %d\n", helper2(p1));
-    printf("num2 - 5 is %d\n", otherHelper(p2));
+    printf("num2 - 5 is %d\n", *p2);
 
-    return 0;
+    int* p3 = NULL;
 
-    /* MAJOR PROBLEM WITH SYNTACTIC MASKING:
-        - Bugs in helper function calls
-    */
+    return *p3;
 }
